6c659c438282b70244dd0926f71cc48f21f8067f
김지훈 <kigpand@gmail.com>
2024-05-08 22:16:44 +0900

    공통 페이지 스타일 분리

--- src/components/main/MainHeader.tsx
+++ src/components/main/MainHeader.tsx
@@ -1,16 +1,5 @@
-import styled from "styled-components";
+import { HeaderWrapper } from "@/styles/PageStyle";
 
 export default function MainHeader() {
   return <HeaderWrapper>오늘의 운동은?</HeaderWrapper>;
 }
-
-const HeaderWrapper = styled.header`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  font-size: 24px;
-  font-weight: bold;
-  background-color: white;
-  color: ${({ theme }) => theme.colors.secondary};
-  padding: 24px 0px;
-`;
