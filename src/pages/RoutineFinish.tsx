557c0795904a5ac49b92c63295e982e46a997d10
김지훈 <kigpand@gmail.com>
2024-05-21 22:22:47 +0900

    feat: 운동 종료 UI 구현

--- src/pages/RoutineFinish.tsx
+++ src/pages/RoutineFinish.tsx
@@ -1,5 +1,42 @@
+import { PATH } from "@/enum/path";
+import { FooterWrapper, HeaderWrapper, PageWrapper } from "@/styles/PageStyle";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export default function RoutineFinish() {
-  return <div>루틴 끝</div>;
+  const nav = useNavigate();
+  return (
+    <PageWrapper>
+      <HeaderWrapper>운동 끝!</HeaderWrapper>
+      <RoutineFinishWrapper>
+        <FinishButton onClick={() => nav(PATH.home)}>처음으로</FinishButton>
+      </RoutineFinishWrapper>
+      <Footer>고생하셨습니다!</Footer>
+    </PageWrapper>
+  );
 }
+
+const FinishButton = styled.button`
+  width: 150px;
+  height: 70px;
+  outline: none;
+  border: none;
+  background-color: #45bbff;
+  color: white;
+  font-size: 18px;
+  border-radius: 12px;
+`;
+
+const RoutineFinishWrapper = styled(PageWrapper)`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Footer = styled(FooterWrapper)`
+  padding: 20px 0px;
+  font-size: 18px;
+  font-weight: bold;
+  background-color: white;
+`;
