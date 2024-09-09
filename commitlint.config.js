d80d855d828f9db1664c9fe4685f98e2c13052bc
김지훈 <kigpand@gmail.com>
2024-09-09 21:57:40 +0900

    fix:xv dfdsf

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,1 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    // 규칙을 작성합니다.
-    // e.g.
-    "type-case": [2, "always", "lower-case"],
-  },
-};
+export default { extends: ["@commitlint/config-conventional"] };
