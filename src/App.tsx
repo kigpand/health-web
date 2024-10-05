5a4d56555b43b9f2137332de99d67aaa045c0473
김지훈 <kigpand@gmail.com>
2024-10-05 19:26:49 +0900

    feat: 개발서버 네트워크 주소로 실행하도록 명령어 수정, query retry 1로 제한

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Error from "./pages/Error";
 import LoadingLayout from "./common/layout/LoadingLayout";
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
 
 function App() {
   return (
