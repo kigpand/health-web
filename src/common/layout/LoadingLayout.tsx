60692bcd6dd68a944f3177d7ebe8fe809b363b37
김지훈 <kigpand@gmail.com>
2025-06-10 23:27:53 +0900

    refactor: route lazy loading 적용

--- src/common/layout/LoadingLayout.tsx
+++ src/common/layout/LoadingLayout.tsx
@@ -1,19 +1,24 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PATH } from "@/enum/path";
 import { useIsFetching } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AddRoutine from "@/pages/AddRoutine";
-import Category from "@/pages/Category";
-import CheckRoutine from "@/pages/CheckRoutine";
-import Error from "@/pages/Error";
+import useLogin from "@/hook/useLogin";
+import LoadingSpinner from "../LoadingSpinner";
+
+// 즉시 import (초기 로딩에 꼭 필요한 화면)
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import RoutineMain from "@/pages/RoutineMain";
-import RoutineDetail from "@/pages/RoutineDetail";
-import RoutineEdit from "@/pages/RoutineEdit";
-import RoutineFinish from "@/pages/RoutineFinish";
-import RoutinePlay from "@/pages/RoutinePlay";
-import LoadingSpinner from "../LoadingSpinner";
-import useLogin from "@/hook/useLogin";
+
+// lazy import (필요할 때 로딩)
+const AddRoutine = lazy(() => import("@/pages/AddRoutine"));
+const Category = lazy(() => import("@/pages/Category"));
+const CheckRoutine = lazy(() => import("@/pages/CheckRoutine"));
+const RoutineDetail = lazy(() => import("@/pages/RoutineDetail"));
+const RoutineEdit = lazy(() => import("@/pages/RoutineEdit"));
+const RoutineFinish = lazy(() => import("@/pages/RoutineFinish"));
+const RoutinePlay = lazy(() => import("@/pages/RoutinePlay"));
+const Error = lazy(() => import("@/pages/Error"));
 
 export default function LoadingLayout() {
   const isFetching = useIsFetching();
@@ -23,20 +28,22 @@ export default function LoadingLayout() {
     <BrowserRouter>
       {loginState === true && <Login />}
       {isFetching > 0 && <LoadingSpinner />}
-      <Routes>
-        <Route path={PATH.home} element={<Home />} />
-        <Route path={PATH.login} element={<Login />} />
-        <Route path={PATH.addRoutine} element={<AddRoutine />} />
-        <Route path={PATH.routineMain} element={<RoutineMain />} />
-        <Route path={PATH.routineDetail} element={<RoutineDetail />} />
-        <Route path={PATH.routinePlay} element={<RoutinePlay />} />
-        <Route path={PATH.routineFinish} element={<RoutineFinish />} />
-        <Route path={PATH.checkRoutine} element={<CheckRoutine />} />
-        <Route path={PATH.routineEdit} element={<RoutineEdit />} />
-        <Route path={PATH.category} element={<Category />} />
-        <Route path={PATH.error} element={<Error />} />
-        <Route path="*" element={<RoutineMain />} />
-      </Routes>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path={PATH.home} element={<Home />} />
+          <Route path={PATH.login} element={<Login />} />
+          <Route path={PATH.addRoutine} element={<AddRoutine />} />
+          <Route path={PATH.routineMain} element={<RoutineMain />} />
+          <Route path={PATH.routineDetail} element={<RoutineDetail />} />
+          <Route path={PATH.routinePlay} element={<RoutinePlay />} />
+          <Route path={PATH.routineFinish} element={<RoutineFinish />} />
+          <Route path={PATH.checkRoutine} element={<CheckRoutine />} />
+          <Route path={PATH.routineEdit} element={<RoutineEdit />} />
+          <Route path={PATH.category} element={<Category />} />
+          <Route path={PATH.error} element={<Error />} />
+          <Route path="*" element={<RoutineMain />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
