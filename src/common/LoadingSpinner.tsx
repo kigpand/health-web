2a8e5de476c1e472140356a36251f195daada2e3
김지훈 <kigpand@gmail.com>
2025-06-15 15:47:26 +0900

    feat: 로딩 스피너 배경색 제거

--- src/common/LoadingSpinner.tsx
+++ src/common/LoadingSpinner.tsx
@@ -3,7 +3,6 @@ import ModalPortal from "@/ModalPortal";
 export default function LoadingSpinner() {
   return (
     <ModalPortal
-      background="rgba(255,255,255,0.3)"
       component={
         <div className="h-[60px] w-[60px] border-4 border-lightgray border-t-0 border-r-0 rounded-full my-4 mx-auto z-[100] animate-spin" />
       }
